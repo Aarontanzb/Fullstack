722256a97e41c5b5fa180eac9ce2f51f6f2d3288
Aaron Tan <aarontanzb@gmail.com>
2023-01-01 21:13:55 +0800

    completed unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,29 +1,37 @@
 import { useState } from 'react'
 
-
+const StatisticLine = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
 
 const Statistics = ({ good, neutral, bad }) => {
   const sum = good + neutral + bad
 
-  return(
-    <> 
-      <p>good {good}</p>
-      <p>neutral {good}</p>
-      <p>bad {good}</p>
-      <p>all {sum}</p>
-      <p>average {(good-bad)/(sum)}</p>
-      <p>positive {String(good/(sum)) + ' %'}</p>
-    </>
+  if (sum === 0) {
+    return (
+      "No feedback given"
+    )
+  }
+
+  return (
+    <table>
+      <tbody> 
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value ={neutral} />
+        <StatisticLine text="bad" value ={bad} />
+        <StatisticLine text="all" value ={sum} />
+        <StatisticLine text="average" value ={(good-bad)/(sum)} />
+        <StatisticLine text="positive" value ={String(good/(sum)*100) + ' %'} />
+      </tbody>
+      </table>
   )
 }
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
   const increment = (state, setState) => () => setState(state + 1)
 
