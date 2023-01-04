eba3fcda9f7317e6eaafc7e0e68a590d6d3ed2e4
Aaron Tan <aarontanzb@gmail.com>
2023-01-05 02:46:56 +0800

    completed countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const Filter = ({ query, handleChange }) => 
+    <p>
+        find countries <input value={query} onChange={handleChange} />
+    </p>
+
+const Country = ({ country }) => <li>{country.name} <button>show</button></li>
+
+const Countries = ({ countries }) =>
+    <ul>
+        {countries.map(country => <Country key={country.name} country={country} />)}
+    </ul>
+
+const CountryInfo = ({ country }) => (
+    <>
+      <h1>{country.name}</h1>
+      <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
+      <h3>languages:</h3>
+      <ul>
+        {Object.values(country.languages).map(language => (
+          <li key={language}>{language}</li>))}
+      </ul>
+      <img
+        src={country.flags.png}
+        alt={`${country.name} flag`}
+      />
+    </>
+)
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [newSearch, setNewSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => 
+        setCountries(response.data.map(({ name, capital, area, languages, flags }) => ({
+          name: name.common,
+          capital,
+          area,
+          languages,
+          flags,
+        })))
+      )
+    }, [])
+
+  const handleChange = setValue => e => setValue(e.target.value)
+
+  const countriesToShow = countries.filter(country => country.name.toLowerCase().includes(newSearch.toLowerCase()))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Filter query={newSearch} handleChange={handleChange(setNewSearch)} />
+
+      {(countriesToShow.length > 10) && (<div>Too many matches, specify another filter</div>)}
+      {(countriesToShow.length <= 10) && (countriesToShow.length > 1) && (<Countries countries={countriesToShow} />)}
+      {(countriesToShow.length === 1) && (<CountryInfo country={countriesToShow[0]} />)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,26 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  const addNote = (event) => {
-    event.preventDefault()
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
+
+  const addNote = (e) => {
+    e.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
+
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
 
-  const handleNoteChange = (event) => setNewNote(event.target.value)
-  
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
 
   return (
     <div>
@@ -29,16 +44,17 @@ const App = (props) => {
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
-      </div>
+      </div>   
       <ul>
         {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input 
+        <input
           value={newNote}
-          onChange={handleNoteChange}/>
+          onChange={handleNoteChange}
+        />
         <button type="submit">save</button>
       </form>
     </div>
