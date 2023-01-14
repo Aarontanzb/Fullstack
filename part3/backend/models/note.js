da20c5bf6c47dbdd4e8880005d070e4e2ccce4e7
Aaron Tan <aarontanzb@gmail.com>
2023-01-14 20:15:00 +0800

    4.1 done

--- part3/backend/models/note.js
+++ part3/backend/models/note.js
@@ -1,27 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.set('useFindAndModify', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     required: true,
     minlength: 5
   },
-  date: Date,
-  important: Boolean,
+  date: {
+    type: Date,
+    required: true
+  },
+  important: Boolean
 })
 
 noteSchema.set('toJSON', {
@@ -31,5 +20,5 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
-  
+
 module.exports = mongoose.model('Note', noteSchema)
