4e721619db7365dca2e36a8d88427e32da0aeb0a
Aaron Tan <aarontanzb@gmail.com>
2023-02-04 00:57:05 +0800

    6.10

--- part6/anecdotes/src/index.js
+++ part6/anecdotes/src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
   reducer: {
-    notes: noteReducer,
+    anecdotes: anecdoteReducer,
     filter: filterReducer,
   },
 })
+
+store.subscribe(() => console.log(store.getState()))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
