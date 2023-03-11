dfc20d472a940c1a955b77193e7522be55ac2e6e
Aaron Tan <aarontanzb@gmail.com>
2023-03-11 11:39:52 +0800

    update eslint

--- part11/.eslintrc.js
+++ part11/.eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    'jest/globals': true,
     node: true,
+    'jest/globals': true,
     'cypress/globals': true
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
@@ -14,10 +14,10 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  plugins: ['react', 'jest'],
+  plugins: ['react', 'jest', 'cypress'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
     eqeqeq: 'error',
